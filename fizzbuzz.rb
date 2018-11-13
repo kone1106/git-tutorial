529a6bcc0ad96be0f00a14212b4ce0ab0aef7082
SenoKota <kehm1229@yahoo.co.jp>
2018-11-13 17:36:23 +0900

    ADD fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,11 @@
 for i in 1..100
 	if i % 3 == 0 && i % 5 == 0 then
 		print("FizzBuzz\n")
+	elsif i % 3 == 0 then
+		print("Fizz\n")
+	elsif i % 5 == 0 then
+		print("Buzz\n")
+	else
+		print("%d\n")
 	end
 end
